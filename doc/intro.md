da8d6020c5b77b9f6f8124f36645d432c54e308f
Martin Barksten <martin.barksten@gmail.com>
2016-02-17 12:40:51 +0100

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to ambig-index-finder
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/ambig_index_finder/core_test.clj
@@ -0,0 +1,7 @@
+(ns ambig-index-finder.core-test
+  (:require [clojure.test :refer :all]
+            [ambig-index-finder.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
