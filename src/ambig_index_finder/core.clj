6956ea71235115c575a017e397e40cbc22f6e664
Martin Barksten <martin.barksten@gmail.com>
2016-02-24 11:23:50 +0100

    Generalize how tests are ran

--- src/ambig_index_finder/core.clj
+++ src/ambig_index_finder/core.clj
@@ -7,8 +7,19 @@
                 (slurp
                   (environ/env :db-config-file))))
 
+(defn- compare-query [db-spec id sample-sizes]
+  (let [sample-size (first sample-sizes)
+        r (rest sample-sizes)
+        result (queries/sample-and-query db-spec id sample-size)]
+    (do
+      (log/info "Sampling with" sample-size "for query with id" id)
+      (log/info result)
+      (if (empty? r)
+        [result]
+        (cons result
+              (compare-query db-spec id r))))))
+
 (defn -main []
-  (log/info db-specs)
-  (queries/compare-query (:postgresql db-specs) "20a" 1 1000))
+  (compare-query (:postgresql db-specs) "1a" [1 10 100]))
 
 ; postmaster -D /usr/local/var/postgres
--- src/ambig_index_finder/queries.clj
+++ src/ambig_index_finder/queries.clj
@@ -1,11 +1,15 @@
 (ns ambig-index-finder.queries
   (:require [clojure.java.jdbc :as j]
-            [clojure.data.json :as json]))
+            [clojure.data.json :as json]
+            [clojure.tools.logging :as log]))
 
 (def query-dir "resources/job")
 
 (defn- resample-with! [db-spec n]
-  (letfn [(exec! [& s] (j/execute! db-spec [(apply str s)]))]
+  (letfn [(exec! [& s]
+                 (do
+                   (log/debug "Executing non-select query" s)
+                   (j/execute! db-spec [(apply str s)])))]
     (exec! "DELETE FROM pg_statistic;")
     (exec! "SET default_statistics_target TO " n ";")
     (exec! "ANALYZE;")))
@@ -13,25 +17,23 @@
 (defn- read-query [query-id]
   (slurp (str query-dir "/" query-id ".sql")))
 
+(defn- execute-query [db-spec query]
+  (do
+    (log/debug "Executing select-query" query)
+    (j/query db-spec query)))
+
 (defn- explain-query [db-spec id]
   (->> id
        (read-query)
        (str "EXPLAIN (ANALYZE TRUE, FORMAT JSON, VERBOSE TRUE) ")
        (vector)
-       (j/query db-spec)
+       (execute-query db-spec)
        (first)
        ((keyword "query plan"))
        (.getValue)
        (json/read-str)))
 
-(defn- sample-and-query [db-spec id n]
+(defn sample-and-query [db-spec id n]
   (do
     (resample-with! db-spec n)
-    (println (json/write-str (explain-query db-spec id)))
-    (println (read-query id))
-    (println (j/query db-spec ["SELECT n_distinct, null_frac, correlation FROM pg_stats;"]))))
-
-(defn compare-query [db-spec id n1 n2]
-  (do
-    (sample-and-query db-spec id n1)
-    (sample-and-query db-spec id n2)))
+    (explain-query db-spec id)))
