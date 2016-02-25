627ac996a29b8f86c27c8bbb266c582fbf4b600c
Martin Barksten <martin.barksten@gmail.com>
2016-02-25 09:56:11 +0100

    Save output to file

--- src/ambig_index_finder/core.clj
+++ src/ambig_index_finder/core.clj
@@ -4,11 +4,16 @@
             [environ.core :as environ]
             [clojure.data.json :as json]
             [clojure.tools.cli :refer [parse-opts]]
-            [clojure.string :refer [split]]))
+            [clojure.string :refer [split]]
+            [clojure.java.io :as io]))
 
-(def db-specs (load-string
-                (slurp
-                  (environ/env :db-config-file))))
+(def output-file (str "output/execution-" (quot (System/currentTimeMillis) 1000)))
+(def output-writer (io/writer output-file))
+
+(def db-specs (load-string (slurp (environ/env :db-config-file))))
+
+(defn save-to-file [s]
+  (.write output-writer (str s "\n")))
 
 (defn- repeat-query [db-spec id repetitions sample-size]
   (repeatedly
@@ -18,25 +23,11 @@
 (defn- compare-query [db-spec id repetitions sample-sizes]
   (let [sample-size (first sample-sizes)
         r (rest sample-sizes)
-        results (repeat-query db-spec id repetitions sample-size)
-        key-val [(keyword (str "sample-" sample-size)) results]]
-    (do
-      (log/debug results)
-      (if (empty? r)
-        (seq [key-val])
-        (cons
-          key-val
-          (compare-query db-spec id repetitions r))))))
-
-(defn evaluate-query [db-name query-id repetitions sample-sizes]
-  (do
-    (log/info "Evaluating query" query-id "for db" db-name "repeating" repetitions "times, with sample sizes" sample-sizes)
-    (let [result (json/write-str
-                   (into {} (compare-query (db-name db-specs)
-                                           query-id
-                                           repetitions
-                                           sample-sizes)))]
-      (log/info result))))
+        results (repeat-query db-spec id repetitions sample-size)]
+    (doall (map #(save-to-file (json/write-str %)) results))
+    (if (empty? r)
+      nil
+      (compare-query db-spec id repetitions r))))
 
 (def cli-options
   [["-qid" "--queryid QID" "Query id"]
@@ -48,12 +39,18 @@
     :parse-fn keyword]])
 
 (defn -main [& args]
-  (let [opts (:options (parse-opts args cli-options))]
-    (evaluate-query
-      (:database opts)
-      (:queryid opts)
-      (:repetitions opts)
-      (:samplesizes opts))))
+  (try
+    (let [opts (:options (parse-opts args cli-options))]
+      (log/info "Executing with parameters:" (json/write-str opts))
+      (log/info "The results of this execution are saved in" output-file)
+      (save-to-file (json/write-str opts))
+      (compare-query
+        ((:database opts) db-specs)
+        (:queryid opts)
+        (:repetitions opts)
+        (:samplesizes opts))
+      (log/info "Execution finished"))
+    (finally (.close output-writer))))
 
 ; postmaster -D /usr/local/var/postgres
 ; lein run --queryid=1a --repetitions=2 --samplesizes='1 2' --database=postgresql
